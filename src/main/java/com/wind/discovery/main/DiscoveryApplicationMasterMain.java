a188360f1cf9613c2b754a82e99ea593e965853a
qianchun <qianchun@huli.com>
2017-10-12 17:07:02 +0800

    init

---
+++ src/main/java/com/wind/discovery/main/DiscoveryApplicationMasterMain.java
@@ -0,0 +1,14 @@
+package com.wind.discovery.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryApplicationMasterMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplicationMasterMain.class, args);
+	}
+}
