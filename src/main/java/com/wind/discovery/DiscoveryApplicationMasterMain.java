083bf4606f0d44b2cf990da8e7144ba94d753dea
qianchun <qianchun@huli.com>
2017-10-25 14:46:57 +0800

    init

--- src/main/java/com/wind/discovery/DiscoveryApplicationMasterMain.java
+++ src/main/java/com/wind/discovery/DiscoveryApplicationMasterMain.java
@@ -1,4 +1,4 @@
-package com.wind.discovery.main;
+package com.wind.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
