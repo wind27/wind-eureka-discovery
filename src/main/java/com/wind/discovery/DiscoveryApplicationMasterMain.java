181074b6379638184b4be29794f8b25bfc46aaf5
qianchun <qianchun@huli.com>
2017-10-25 14:54:50 +0800

    init

--- src/main/java/com/wind/discovery/DiscoveryApplicationMasterMain.java
+++ src/main/java/com/wind/discovery/DiscoveryApplicationMasterMain.java
@@ -2,8 +2,15 @@ package com.wind.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.*;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Import;
 
+@Import({ DispatcherServletAutoConfiguration.class, EmbeddedServletContainerAutoConfiguration.class,
+		ErrorMvcAutoConfiguration.class, HttpEncodingAutoConfiguration.class,
+		HttpMessageConvertersAutoConfiguration.class, JacksonAutoConfiguration.class, MultipartAutoConfiguration.class,
+		ServerPropertiesAutoConfiguration.class, WebMvcAutoConfiguration.class })
 @EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryApplicationMasterMain {
